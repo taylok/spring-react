160b3c93cf766b542586f5fbec2fca6ed6fdc7f4
Keith Taylor <taylor.km61@icloud.com>
2023-10-30 12:20:21 +0000

    React Building Customers component

--- wisdom-pet/src/ui/src/App.js
+++ wisdom-pet/src/ui/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
+import Customers from './pages/Customers'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path={'/'} element={<Layout/>}>
             <Route index element={<Home/>}/>
+            <Route path={'customers'} element={<Customers/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
